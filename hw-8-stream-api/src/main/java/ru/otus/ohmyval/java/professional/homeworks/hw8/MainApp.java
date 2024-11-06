6e30333e484357d61c43658c1d836b22b33f50dc
ohmyval <makeup-style@mail.ru>
2024-11-07 00:37:59 +0300

    Methods extacted HW-8 done

--- hw-8-stream-api/src/main/java/ru/otus/ohmyval/java/professional/homeworks/hw8/MainApp.java
+++ hw-8-stream-api/src/main/java/ru/otus/ohmyval/java/professional/homeworks/hw8/MainApp.java
@@ -2,7 +2,7 @@ package ru.otus.ohmyval.java.professional.homeworks.hw8;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class MainApp {
     public static void main(String[] args) {
@@ -10,77 +10,61 @@ public class MainApp {
         TaskApplication taskApp = new TaskApplication();
 
         List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
-//        System.out.println(myTaskList);
-//
-//
-//        List<TaskApplication.MyTask> myTasksInWork = myTaskList.stream()
-//                .filter(myTask -> myTask.status.equals(Status.INWORK))
-//                .toList();
-        System.out.println(myTasksInWork(taskApp));
-        System.out.println(closedTasksQuantity(taskApp));
-        System.out.println(ifTaskExist(taskApp));
 
-
-//        Long closedTasksQuantity = myTaskList.stream()
-//                .filter(myTask -> myTask.status.equals(Status.CLOSED))
-//                .count();
-//        System.out.println(closedTasksQuantity);
-
-
-//        boolean taskId2Exist = myTaskList.stream()
-//                .anyMatch(myTask -> myTask.getId() == 2);
-//        boolean taskId99NotExist = myTaskList.stream()
-//                .noneMatch(myTask -> myTask.getId() == 99);
-//        List<Boolean> result = Arrays.asList(taskId2Exist,taskId99NotExist);
-//        System.out.println(result);
-
-
-        List<TaskApplication.MyTask> myTaskSortedList = myTaskList.stream()
-                .sorted(Comparator.comparing(myTask -> myTask.status))
-                .toList();
-        System.out.println(myTaskSortedList);
-
-
-        Map<Status, Map<String, List<TaskApplication.MyTask>>> tasksGroupedByStatusAndId = myTaskList.stream()
-                .collect(Collectors.groupingBy(TaskApplication.MyTask::getStatus,
-                        Collectors.groupingBy(myTask -> {
-                            myTask.setParity();
-                            return myTask.getParity();
-                        })));
-        System.out.println(tasksGroupedByStatusAndId);
-
-
-        Map<Boolean, List<TaskApplication.MyTask>> tasksDividedByStatus = myTaskList.stream()
-                .collect(Collectors.partitioningBy(myTask -> myTask.status.equals(Status.CLOSED)));
-        System.out.println(tasksDividedByStatus);
-
+        System.out.println("myTaskList: " + myTaskList);
+        System.out.println("myTasksInWork: " + myTasksInWork(taskApp));
+        System.out.println("closedTasksQuantity: " + closedTasksQuantity(taskApp));
+        System.out.println("ifTaskExist: " + ifTaskExist(taskApp));
+        System.out.println("myTaskSortedList: " + myTaskSortedList(taskApp));
+        System.out.println("tasksGroupedByStatusAndId: " + tasksGroupedByStatusAndId(taskApp));
+        System.out.println("tasksDividedByStatus: " + tasksDividedByStatus(taskApp));
     }
 
-    public static List<TaskApplication.MyTask> myTasksInWork(TaskApplication taskApp){
+    public static List<TaskApplication.MyTask> myTasksInWork(TaskApplication taskApp) {
         List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
         return myTaskList.stream()
                 .filter(myTask -> myTask.status.equals(Status.INWORK))
                 .toList();
-
     }
-    public static Long closedTasksQuantity(TaskApplication taskApp){
+
+    public static Long closedTasksQuantity(TaskApplication taskApp) {
         List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
         return myTaskList.stream()
                 .filter(myTask -> myTask.status.equals(Status.CLOSED))
                 .count();
     }
 
-    public static List<Boolean> ifTaskExist(TaskApplication taskApp){
+    public static List<Boolean> ifTaskExist(TaskApplication taskApp) {
         List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
         boolean taskId2Exist = myTaskList.stream()
                 .anyMatch(myTask -> myTask.getId() == 2);
         boolean taskId99NotExist = myTaskList.stream()
                 .noneMatch(myTask -> myTask.getId() == 99);
-        List<Boolean> result = Arrays.asList(taskId2Exist,taskId99NotExist);
-        return result;
+        return Arrays.asList(taskId2Exist, taskId99NotExist);
     }
 
+    public static List<TaskApplication.MyTask> myTaskSortedList(TaskApplication taskApp) {
+        List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
+        return myTaskList.stream()
+                .sorted(Comparator.comparing(myTask -> myTask.status))
+                .toList();
+    }
 
+    public static Map<Status, Map<String, List<TaskApplication.MyTask>>> tasksGroupedByStatusAndId(TaskApplication taskApp) {
+        List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
+        return myTaskList.stream()
+                .collect(Collectors.groupingBy(TaskApplication.MyTask::getStatus,
+                        Collectors.groupingBy(myTask -> {
+                            myTask.setParity();
+                            return myTask.getParity();
+                        })));
+    }
 
+    public static Map<Boolean, List<TaskApplication.MyTask>> tasksDividedByStatus(TaskApplication taskApp) {
+        List<TaskApplication.MyTask> myTaskList = taskApp.getMyTaskList();
+        return myTaskList.stream()
+                .collect(Collectors.partitioningBy(myTask -> myTask.status.equals(Status.CLOSED)));
+
+    }
 
 }
