f9e63e369e2d02d309d6396e203068d6acf91c07
ohmyval <makeup-style@mail.ru>
2024-12-10 18:13:06 +0300

    init commit all classes created

---
+++ hw-15-pattern-iterator/src/main/java/ru/otus/ohmyval/java/professional/homeworks/hw15/MainApp.java
@@ -0,0 +1,7 @@
+package ru.otus.ohmyval.java.professional.homeworks.hw15;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ hw-15-pattern-iterator/src/main/java/ru/otus/ohmyval/java/professional/homeworks/hw15/Matryoshka.java
@@ -0,0 +1,12 @@
+package ru.otus.ohmyval.java.professional.homeworks.hw15;
+
+
+import java.util.List;
+
+public final class Matryoshka {
+    private final List<String> items;
+
+    public Matryoshka(List<String> items) {
+        this.items = items;
+    }
+}
