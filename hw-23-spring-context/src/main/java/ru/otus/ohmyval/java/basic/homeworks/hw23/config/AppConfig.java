9f0609bd8b20eff9c60f2b9d2e637c4a1df268cf
ohmyval <makeup-style@mail.ru>
2025-06-04 14:55:16 +0300

    hw 23 changed after PR

--- hw-23-spring-context/src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw23/config/AppConfig.java
+++ hw-23-spring-context/src/main/java/ru/otus/ohmyval/java/basic/homeworks/hw23/config/AppConfig.java
@@ -2,6 +2,7 @@ package ru.otus.ohmyval.java.basic.homeworks.hw23.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import ru.otus.ohmyval.java.basic.homeworks.hw23.sevices.Cart;
 import ru.otus.ohmyval.java.basic.homeworks.hw23.sevices.ProductRepository;
 import ru.otus.ohmyval.java.basic.homeworks.hw23.sevices.impl.CartImpl;
@@ -11,6 +12,7 @@ import ru.otus.ohmyval.java.basic.homeworks.hw23.sevices.impl.CartImpl;
 public class AppConfig {
 
     @Bean
+    @Scope("prototype")
     public Cart cart(ProductRepository productRepository) {
         return new CartImpl(productRepository);
     }
